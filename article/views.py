37b06a909d2b4603244b0d1437a81c5a5f520f8d
wolfgnahz <5573468+wolfgnahz@user.noreply.gitee.com>
2024-05-11 23:01:04 +0800

    blog add markdown style

--- article/views.py
+++ article/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ArticlePost
+import markdown
 # Create your views here.
 
 
@@ -17,6 +18,14 @@ def article_list(request):
 def article_detail(request, id):
     # 取出相应的文章
     article = ArticlePost.objects.get(id=id)
+    # 将markdown语法渲染成html样式
+    article.body = markdown.markdown(article.body,
+		extensions=[
+		# 包含 缩写、表格等常用扩展
+		'markdown.extensions.extra',
+		# 语法高亮扩展
+		'markdown.extensions.codehilite',
+		])
     # 需要传递给模板的对象
     context = { 'article': article }
     # 载入模板，并返回context对象
