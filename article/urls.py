4395a1125447128d16349feea50f577a68f54c28
wolfgnahz <5573468+wolfgnahz@user.noreply.gitee.com>
2024-05-18 16:24:36 +0800

    增加文章提交页面 & 在首页增加写文章入口

--- article/urls.py
+++ article/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     # path函数将url映射到视图
     path('article-list/', views.article_list, name='article_list'),
     path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
+    # 写文章
+    path('article-create/', views.article_create, name='article_create'),
 ]
